047e64ee8bb5e060feb5dcb714f6173ed0742c65
Jorge Marcelo De Souza Júnior <BRX0001455@latam2.prosegur.local>
2019-08-22 13:28:30 -0300

    Correcao issues Copia de arquivos de Nivel de pastas. Alterado forma como retorna os diretorios

--- hot.py
+++ hot.py
@@ -5,31 +5,21 @@ import sys
 from git import Repo
 
 def dirGit():
-    dir = 'C:\\GIT\\SOL\\'
-    return dir
+    return os.path.abspath('C:\\GIT\\SOL\\')
 
 def pathWeblogic():
-    return 'C:\\ambiente\\sw\\Oracle\\Middleware12212\\Oracle_Home\\user_projects\\domains\\cash\\servers\\AdminServer\\tmp\\_WL_user\\sol-base-ear-5.5.0-SNAPSHOT-DEV'
+    return os.path.abspath('C:\\ambiente\\sw\\Oracle\\Middleware12212\\Oracle_Home\\user_projects\\domains\\cash\\servers\\AdminServer\\tmp\\_WL_user\\sol-base-ear-5.5.0-SNAPSHOT-DEV')
 
 def openBrowser():
     browser = webdriver.Chrome() 
     browser.get('http://localhost:7001/sol')
 
-def find(fileFonte):
-    dirs = os.listdir(pathWeblogic())
-    for path in dirs:
-        dir = os.listdir(pathWeblogic() +'\\'+ path)
-        for warDir in dir:
-            if warDir == "war":
-                warPath = pathWeblogic() +'\\'+ path + '\\' + warDir
-                war = os.listdir(warPath)
-                for dirPage in war:
-                    if (dirPage == 'pages'):
-                        pagesPath = warPath + '\\' + dirPage
-                        pages = os.listdir(pagesPath)
-                        for file in pages:
-                            if fileFonte == file:
-                                return pagesPath + '\\' + file
+# Percorre todo o diretório do servido para encontrar os arquivos
+def findFile(fileFonte):
+    files = []
+    for pastaAtual, subPasta, arquivos in os.walk(pathWeblogic()):
+        files.extend([os.path.join(pastaAtual, arquivo) for arquivo in arquivos if arquivo == fileFonte])
+    return files                                
 
 def repo():
     repo = Repo('C:\\GIT\\SOL\\')
@@ -49,10 +39,15 @@ def copy():
     files = gitDiff()
     if len(files) > 0:
         for item in files:
-            fileName = item.split('/')
-            fonte = dirGit() + item
-            destino = find(fileName[-1])
-            cp.copyfile(fonte, destino)
+            pathFile = item.split('/')
+            fileName = pathFile[-1]
+            fonte = dirGit() + '/' + item
+            destino = findFile(fileName)[0]
+            try:
+                cp.copyfile(fonte, destino)
+                print(fileName + " copiado com sucesso")
+            except:
+                print("Erro ao copiar o arquivo " + fileName)
     else:
         print("Nenhum arquivo para realizar deploy!")
 
